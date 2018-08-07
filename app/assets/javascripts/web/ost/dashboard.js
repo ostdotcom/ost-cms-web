e43bc7f3ecaf3634138750e4d2234e6d833198b2
Akshay Raje <akshay.raje@gmail.com>
2018-08-07 20:50:18 +0530

    Handlebars based form

--- app/assets/javascripts/web/ost/dashboard.js
+++ app/assets/javascripts/web/ost/dashboard.js
@@ -1,17 +1,19 @@
 ;
 (function (window) {
 
-  var oSTNs = ns("ost"),
+  var oSTNs = ns("cms"),
       oThis;
 
-  oSTNs.index = oThis = {
+  oSTNs.dashboard = oThis = {
+
+    ostFormBuilder: null,
 
     init: function (config) {
       oThis.bindButtonActions();
+      oThis.ostFormBuilder = new cms.OstFormBuilder();
     },
 
     bindButtonActions: function () {
-
 
       var treeviewMenu = $('.app-menu');
 
@@ -56,6 +58,14 @@
       $('.dropdown').on('hide.bs.dropdown', function(e){
         $(this).find('.dropdown-menu').first().stop(true, true).slideUp(200);
       });
+
+        $('#createModal').on('show.bs.modal', function (e) {
+            oThis.ostFormBuilder.renderTemplate(
+                '#news_list',
+                {news_list: meta_data.meta.news_list},
+                '#createModal .modal-body'
+            );
+        })
 
     }
   };
