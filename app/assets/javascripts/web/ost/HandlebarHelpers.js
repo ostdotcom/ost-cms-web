d7b1bcfbd38e24b9e9403ead4b5f6adc342b4ea2
Ashutosh <ashutosh@ost.com>
2018-09-20 20:16:28 +0530

    custom news list

--- app/assets/javascripts/web/ost/HandlebarHelpers.js
+++ app/assets/javascripts/web/ost/HandlebarHelpers.js
@@ -79,6 +79,17 @@
     return (year+ separator + month + separator + date);
   });
 
+  Handlebars.registerHelper('getDisplayDate', function (val, config , options) {
+    var normalisedVal = parseInt(val) * normaliser ,
+        dateObj       = new Date( normalisedVal ),
+        date          = (dateObj.getUTCDate()),
+        config        = config || {day: 'numeric',month:'long',year:'numeric'}
+    ;
+    return dateObj.toLocaleDateString('en-US', config);
+  });
+
+
+
   Handlebars.registerHelper('ifFilePath', function (data, options) {
     if (typeof data == "string") {
       return options.fn(this);
