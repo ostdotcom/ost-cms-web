be294a962bdba7b9a2555f79667f202ece39ef7d
mayurpatil888 <mayur@ost.com>
2018-10-12 17:37:37 +0530

    exchange entity

--- app/assets/javascripts/web/ost/entity.js
+++ app/assets/javascripts/web/ost/entity.js
@@ -41,6 +41,7 @@
       oThis.getEntityConfig();
       oThis.selectSidebarMenu();
       oThis.hideSideBarMenuItem();
+      oThis.handleSidebar();
     },
 
     initializeData: function ( config ) {
@@ -439,6 +440,25 @@
           $(jEl).closest(".treeview").hide();
         }
       });
+    },
+
+    handleSidebar: function(){
+      var treeviewMenu = $('.app-menu');
+      // Toggle Sidebar
+      $("[data-toggle='sidebar']").click(function (event) {
+        $('.app').toggleClass('sidenav-toggled');
+      });
+      // Activate sidebar treeview toggle
+      $("[data-toggle='treeview']").click(function (event) {
+        event.preventDefault();
+        if (!$(this).parent().hasClass('is-expanded')) {
+          treeviewMenu.find("[data-toggle='treeview']").parent().removeClass('is-expanded');
+        }
+        $(this).parent().toggleClass('is-expanded');
+      });
+
+      // Set initial active toggle
+      $("[data-toggle='treeview.'].is-expanded").parent().toggleClass('is-expanded');
     }
 
   };
